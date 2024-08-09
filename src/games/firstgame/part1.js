7138e1a73b24f82a400255db384e99ae56f822c4
Kevin Savsani <kevinsavsani@Kevins-MacBook-Air.local>
2024-08-09 22:10:47 +0530

    first commit

---
+++ src/games/firstgame/part1.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser';
+
+class Example extends Phaser.Scene
+{
+    preload ()
+    {
+    }
+
+    create ()
+    {
+    }
+
+    update ()
+    {
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: Example
+};
+
+const game = new Phaser.Game(config);
